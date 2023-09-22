0cfefb75bbc700bda7cc6097a560423e81d818eb
unknown <kavimoorthi2003@gmail.com>
2023-09-22 18:17:42 +0530

    connect php file is updated

---
+++ connect.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+$host="localhost";
+$user="root";
+$pass="";
+$db="first_connection";
+//$str="mysql:host=localhost;dbname=first_connection;charset=utf8";
+$str="mysql:host=".$host.";dbname=".$db;
+try{
+   $con=new PDO($str,$user,$pass);
+    $sql="select * from pdo";
+    $res=$con->query($sql);
+    while($row=$res->fetch(PDO::FETCH_ASSOC)){
+        echo $row["email"]."-".$row["user"]."-".$row["pass"]."<br>";
+    }
+   echo "good";
+
+}
+catch(PDOException $e){
+    echo"connection error : ".$e->getMessage();
+}
+
+//$dsn = "mysql:host=localhost;dbname=your_database_name;charset=utf8";
+//$username = "your_username";
+//$password = "your_password";*/
+$email=
+
+?>
